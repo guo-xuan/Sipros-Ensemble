a9380811a43e45fbd734ddb002507b5c68cdc5fa
Xuan Guo <xuan_guo@outlook.com>
2017-03-28 17:55:05 -0400

    V1.0

--- compiler.mk
+++ compiler.mk
@@ -8,8 +8,8 @@ OPTS := -std=c++11 -fopenmp -O3 -g
 
 # MPI C++ Compiler for OpenMPI installations (default)
 # MCC := mpic++
-# MOPTS := -std=c++11 -fopenmp -O3 -g
+# MOPTS := -std=c++11 -openmp -O3 -g
 
 # Intel MPI C++ Compiler installations
-# MCC := icpc
-# MOPTS := -std=c++11 -openmp -O3 -g -lmpi -lmpi++
+MCC := icpc
+MOPTS := -std=c++11 -openmp -O3 -g -lmpi -lmpi++
